662e9cad5a5541a074b47cb765e7d578d41dfcc4
pavel <pavel@myhost>
2017-04-15 21:19:08 +0300

    task5 started

---
+++ task5/pycharm_project/modules.py
@@ -0,0 +1,442 @@
+import numpy as np
+
+
+class Module(object):
+    def __init__(self):
+        self.output = None
+        self.gradInput = None
+        self.training = True
+
+    """
+    Basically, you can think of a module as of a something (black box)
+    which can process `input` data and produce `ouput` data.
+    This is like applying a function which is called `forward`:
+
+        output = module.forward(input)
+
+    The module should be able to perform a backward pass: to differentiate the `forward` function.
+    More, it should be able to differentiate it if is a part of chain (chain rule).
+    The latter implies there is a gradient from previous step of a chain rule.
+
+        gradInput = module.backward(input, gradOutput)
+    """
+
+    def forward(self, input):
+        """
+        Takes an input object, and computes the corresponding output of the module.
+        """
+        return self.updateOutput(input)
+
+    def backward(self, input, gradOutput):
+        """
+        Performs a backpropagation step through the module, with respect to the given input.
+
+        This includes
+         - computing a gradient w.r.t. `input` (is needed for further backprop),
+         - computing a gradient w.r.t. parameters (to update parameters while optimizing).
+        """
+        self.updateGradInput(input, gradOutput)
+        self.accGradParameters(input, gradOutput)
+        return self.gradInput
+
+    def updateOutput(self, input):
+        """
+        Computes the output using the current parameter set of the class and input.
+        This function returns the result which is stored in the `output` field.
+
+        Make sure to both store the data in `output` field and return it.
+        """
+
+        # The easiest case:
+
+        # self.output = input
+        # return self.output
+
+        pass
+
+    def updateGradInput(self, input, gradOutput):
+        """
+        Computing the gradient of the module with respect to its own input.
+        This is returned in `gradInput`. Also, the `gradInput` state variable is updated accordingly.
+
+        The shape of `gradInput` is always the same as the shape of `input`.
+
+        Make sure to both store the gradients in `gradInput` field and return it.
+        """
+
+        # The easiest case:
+
+        # self.gradInput = gradOutput
+        # return self.gradInput
+
+        pass
+
+    def accGradParameters(self, input, gradOutput):
+        """
+        Computing the gradient of the module with respect to its own parameters.
+        No need to override if module has no parameters (e.g. ReLU).
+        """
+        pass
+
+    def zeroGradParameters(self):
+        """
+        Zeroes `gradParams` variable if the module has params.
+        """
+        pass
+
+    def getParameters(self):
+        """
+        Returns a list with its parameters.
+        If the module does not have parameters return empty list.
+        """
+        return []
+
+    def getGradParameters(self):
+        """
+        Returns a list with gradients with respect to its parameters.
+        If the module does not have parameters return empty list.
+        """
+        return []
+
+    def training(self):
+        """
+        Sets training mode for the module.
+        Training and testing behaviour differs for Dropout, BatchNorm.
+        """
+        self.training = True
+
+    def evaluate(self):
+        """
+        Sets evaluation mode for the module.
+        Training and testing behaviour differs for Dropout, BatchNorm.
+        """
+        self.training = False
+
+    def __repr__(self):
+        """
+        Pretty printing. Should be overrided in every module if you want
+        to have readable description.
+        """
+        return "Module"
+
+
+class Sequential(Module):
+    """
+         This class implements a container, which processes `input` data sequentially.
+
+         `input` is processed by each module (layer) in self.modules consecutively.
+         The resulting array is called `output`.
+    """
+
+    def __init__(self):
+        super(Sequential, self).__init__()
+        self.modules = []
+
+    def add(self, module):
+        """
+        Adds a module to the container.
+        """
+        self.modules.append(module)
+
+    def updateOutput(self, input):
+        """
+        Basic workflow of FORWARD PASS:
+
+            y_0    = module[0].forward(input)
+            y_1    = module[1].forward(y_0)
+            ...
+            output = module[n-1].forward(y_{n-2})
+
+
+        Just write a little loop.
+        """
+
+        ################################################
+        self.output = input
+        for module in self.modules:
+            self.output = module.forward(self.output)
+            ################################################
+        return self.output
+
+    def backward(self, input, gradOutput):
+        """
+        Workflow of BACKWARD PASS:
+
+            g_{n-1} = module[n-1].backward(y_{n-2}, gradOutput)
+            g_{n-2} = module[n-2].backward(y_{n-3}, g_{n-1})
+            ...
+            g_1 = module[1].backward(y_0, g_2)
+            gradInput = module[0].backward(input, g_1)
+
+
+        !!!
+
+        To ech module you need to provide the input, module saw while forward pass,
+        it is used while computing gradients.
+        Make sure that the input for `i-th` layer the output of `module[i]` (just the same input as in forward pass)
+        and NOT `input` to this Sequential module.
+
+        !!!
+
+        """
+        ################################################
+        self.gradInput = gradOutput
+        inputs = [input]
+        for module in self.modules:
+            inputs.append(module.forward(self.output))
+        for module in self.modules[::-1]:
+            self.gradInput = module.backward(inputs.pop(), self.gradInput)
+        ################################################
+        return self.gradInput
+
+    def zeroGradParameters(self):
+        for module in self.modules:
+            module.zeroGradParameters()
+
+    def getParameters(self):
+        """
+        Should gather all parameters in a list.
+        """
+        return [x.getParameters() for x in self.modules]
+
+    def getGradParameters(self):
+        """
+        Should gather all gradients w.r.t parameters in a list.
+        """
+        return [x.getGradParameters() for x in self.modules]
+
+    def __repr__(self):
+        string = "".join([str(x) + '\n' for x in self.modules])
+        return string
+
+    def __getitem__(self, x):
+        return self.modules.__getitem__(x)
+
+#####################################
+########## layers ###################
+#####################################
+class Linear(Module):
+    """
+    A module which applies a linear transformation
+    A common name is fully-connected layer, InnerProductLayer in caffe.
+
+    The module should work with 2D input of shape (n_samples, n_feature).
+    """
+
+    def __init__(self, n_in, n_out):
+        super(Linear, self).__init__()
+
+        # This is a nice initialization
+        stdv = 1. / np.sqrt(n_in)
+        self.W = np.random.uniform(-stdv, stdv, size=(n_out, n_in))
+        self.b = np.random.uniform(-stdv, stdv, size=n_out)
+
+        self.gradW = np.zeros_like(self.W)
+        self.gradb = np.zeros_like(self.b)
+
+    def updateOutput(self, input):
+        ################################################
+        self.output = np.dot(input, self.W) + self.b
+        ################################################
+        return self.output
+
+    def updateGradInput(self, input, gradOutput):
+        ################################################
+        self.gradInput = np.dot(gradOutput, self.W.transpose())
+        ################################################
+        return self.gradInput
+
+    def accGradParameters(self, input, gradOutput):
+        ################################################
+        self.gradW = np.dot(gradOutput.transpose(), input)
+        self.gradb = np.sum(gradOutput, axis=0)
+        print '\n grad:'
+        print self.gradW
+        print self.gradb
+        print '\n'
+        ################################################
+        pass
+
+    def zeroGradParameters(self):
+        self.gradW.fill(0)
+        self.gradb.fill(0)
+
+    def getParameters(self):
+        return [self.W, self.b]
+
+    def getGradParameters(self):
+        return [self.gradW, self.gradb]
+
+    def print_W(self):
+        print self.W
+
+    def __repr__(self):
+        s = self.W.shape
+        q = 'Linear %d -> %d' % (s[1], s[0])
+        return q
+
+
+class SoftMax(Module):
+    def __init__(self):
+        super(SoftMax, self).__init__()
+
+    def updateOutput(self, input):
+        # start with normalization for numerical stability
+        self.output = np.subtract(input, input.max(axis=1, keepdims=True))
+        self.output = np.exp(self.output)
+        self.output /= self.output.sum(axis=1)
+        ################################################
+        ################################################
+        return self.output
+
+    def updateGradInput(self, input, gradOutput):
+        # Your code goes here. ################################################
+        return self.gradInput
+
+    def __repr__(self):
+        return "SoftMax"
+
+
+class Dropout(Module):
+    def __init__(self, p=0.5):
+        super(Dropout, self).__init__()
+
+        self.p = p
+        self.mask = None
+
+    def updateOutput(self, input):
+        # Your code goes here. ################################################
+        return self.output
+
+    def updateGradInput(self, input, gradOutput):
+        # Your code goes here. ################################################
+        return self.gradInput
+
+    def __repr__(self):
+        return "Dropout"
+
+
+#####################################
+####### activation functions ########
+#####################################
+class ReLU(Module):
+    def __init__(self):
+        super(ReLU, self).__init__()
+
+    def updateOutput(self, input):
+        self.output = np.maximum(input, 0)
+        return self.output
+
+    def updateGradInput(self, input, gradOutput):
+        self.gradInput = np.multiply(gradOutput, input > 0)
+        return self.gradInput
+
+    def __repr__(self):
+        return "ReLU"
+
+
+class LeakyReLU(Module):
+    def __init__(self, slope=0.03):
+        super(LeakyReLU, self).__init__()
+
+        self.slope = slope
+
+    def updateOutput(self, input):
+        # Your code goes here. ################################################
+        return self.output
+
+    def updateGradInput(self, input, gradOutput):
+        # Your code goes here. ################################################
+        return self.gradInput
+
+    def __repr__(self):
+        return "LeakyReLU"
+
+
+#####################################
+########## criterions ###############
+#####################################
+
+class Criterion(object):
+    def __init__(self):
+        self.output = None
+        self.gradInput = None
+
+    def forward(self, input, target):
+        """
+            Given an input and a target, compute the loss function
+            associated to the criterion and return the result.
+
+            For consistency this function should not be overrided,
+            all the code goes in `updateOutput`.
+        """
+        return self.updateOutput(input, target)
+
+    def backward(self, input, target):
+        """
+            Given an input and a target, compute the gradients of the loss function
+            associated to the criterion and return the result.
+
+            For consistency this function should not be overrided,
+            all the code goes in `updateGradInput`.
+        """
+        return self.updateGradInput(input, target)
+
+    def updateOutput(self, input, target):
+        """
+        Function to override.
+        """
+        return self.output
+
+    def updateGradInput(self, input, target):
+        """
+        Function to override.
+        """
+        return self.gradInput
+
+    def __repr__(self):
+        """
+        Pretty printing. Should be overrided in every module if you want
+        to have readable description.
+        """
+        return "Criterion"
+
+
+class MSECriterion(Criterion):
+    def __init__(self):
+        super(MSECriterion, self).__init__()
+
+    def updateOutput(self, input, target):
+        self.output = np.sum(np.power(input - target, 2)) / input.shape[0]
+        return self.output
+
+    def updateGradInput(self, input, target):
+        self.gradInput = (input - target) * 2 / input.shape[0]
+        return self.gradInput
+
+    def __repr__(self):
+        return "MSECriterion"
+
+
+class ClassNLLCriterion(Criterion):
+    def __init__(self):
+        a = super(ClassNLLCriterion, self)
+        super(ClassNLLCriterion, self).__init__()
+
+    def updateOutput(self, input, target):
+        # Use this trick to avoid numerical errors
+        eps = 1e-15
+        input_clamp = np.clip(input, eps, 1 - eps)
+
+        # Your code goes here. ################################################
+        return self.output
+
+    def updateGradInput(self, input, target):
+        # Use this trick to avoid numerical errors
+        input_clamp = np.maximum(1e-15, np.minimum(input, 1 - 1e-15))
+
+        # Your code goes here. ################################################
+        return self.gradInput
+
+    def __repr__(self):
+        return "ClassNLLCriterion"
